bc1d7d3fe186ca3256d34c49fbc7ebe6bb96661b
arashma <alidadi_arash@yahoo.com>
2018-12-19 01:28:44 +0330

    Add tests for GET /users/me and POST /users

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -2,20 +2,10 @@ const request     = require('supertest');
 const app         = require('./../app');
 const {Todo}      = require('./../models/todo');
 const {ObjectID}  = require('mongodb');
-
-const todos  = [{
-  _id  : new ObjectID(),
-  text : "First todo text"
-},{
-  _id  : new ObjectID(),
-  text:"second todo text"
-}];
-
-beforeEach((done)=>{
-  Todo.remove({}).then(()=>{
-    return Todo.insertMany(todos);
-  }).then(()=>done());
-});
+const {todos , populateTodos,users,populateUsers} = require('./seed/seed');
+const {User}      = require('./../models/user');
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
     test('It should create a new todo', (done) => {
@@ -108,5 +98,79 @@ describe('GET /todos/:id', () => {
           expect(res.body.todo.text).toBe(todos[0].text);
         })
         .end(done);
+    });
+});
+
+describe('GET /users/me', () => {
+    test('should return user if authenticated', (done) => {
+      request(app)
+      .get(`/users/me`)
+      .set('x-auth',users[0].tokens[0].token)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+    });
+
+    test('should return 401 if not authenticated', (done) => {
+        request(app)
+        .get(`/users/me`)
+        .expect(401)
+        .expect((res)=>{
+          expect(res.body).toEqual({});
+        })
+        .end(done);
+    });
+});
+
+
+describe('POST /users', () => {
+    test('should create a user', (done) => {
+      var email = "arashdevelopppp@gmail.com";
+      var password = "123arash!";
+      request(app)
+      .post(`/users`)
+      .send({email,password})
+      .expect(200)
+      .expect((res)=>{
+          expect(res.headers['x-auth']).not.toBeNull();
+          expect(res.body._id).not.toBeNull();
+          expect(res.body.email).toBe(email);
+      })
+      .end((err)=>{
+        if(err){
+          return done(err);
+        }
+
+        User.findOne({email}).then((user)=>{
+          expect(user).not.toBeNull();
+          expect(user.password).not.toBe(password);
+          done();
+        })
+      });
+    });
+
+    test('should return validation errors if request invalid', (done) => {
+      request(app)
+      .post('/users')
+      .send({
+        email:'and',
+        password:'123'
+      })
+      .expect(400)
+      .end(done)
+    });
+
+    test('should not create a user if email in use', (done) => {
+      request(app)
+      .post('/users')
+      .send({
+        email:users[0].email,
+        password:'Password123'
+      })
+      .expect(400)
+      .end(done)
     });
 });
---
+++ server/tests/seed/seed.js
@@ -0,0 +1,49 @@
+const {ObjectID}  = require('mongodb');
+const {Todo}      = require('./../../models/todo');
+const {User}      = require('./../../models/user');
+const jwt       = require('jsonwebtoken');
+
+const userOneId = new ObjectID();
+const userTowId = new ObjectID();
+const users  = [{
+  _id  : userOneId,
+  email:'arashdeveloper@gmail.com',
+  password:'userOnepass',
+  tokens:[{
+    access:'auth',
+    token:jwt.sign({_id:userOneId,access:'auth'},'abc123').toString()
+  }]
+},{
+  _id  : userTowId,
+  email:'arashdevelopermid@gmail.com',
+  password:'userTowpass',
+  tokens:[{
+    access:'auth',
+    token:jwt.sign({_id:userTowId,access:'auth'},'abc123').toString()
+  }]
+}];
+
+const todos  = [{
+  _id  : new ObjectID(),
+  text : "First todo text"
+},{
+  _id  : new ObjectID(),
+  text:"second todo text"
+}];
+
+const populateTodos = (done)=>{
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos);
+  }).then(()=>done());
+};
+
+const populateUsers = (done)=>{
+  User.remove({}).then(()=>{
+    var userOne = new User(users[0]).save();
+    var userTow = new User(users[1]).save();
+
+    return Promise.all([userOne , userTow])
+  }).then(()=>done());
+}
+
+module.exports = {todos , populateTodos,populateUsers ,users};
